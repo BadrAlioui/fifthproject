3d4f93d4c3af937710601f566199a0447f05240a
Badr Aliloui <tranquil2021@hotmail.com>
2024-11-24 19:15:32 +0000

    Add custom admin configuration for Category and Product models with prepopulated fields

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+#https://www.udemy.com/course/python-django-build-an-e-commerce-store-2022/learn/lecture/34534700?start=180#overview
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
