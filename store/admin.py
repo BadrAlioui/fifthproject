da51b8e69d01493015a92d0d4fca4656ee167275
Badr Aliloui <tranquil2021@hotmail.com>
2024-11-24 19:21:29 +0000

    replaced name by title

--- store/admin.py
+++ store/admin.py
@@ -9,4 +9,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('title',)}
