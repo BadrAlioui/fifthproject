107c087df59f8ec9bb01e29f9b2d11335bcc62a3
Badr Aliloui <tranquil2021@hotmail.com>
2024-11-25 09:18:24 +0000

    Added update and delete option

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.store, name="store"),
     path('product/<slug:slug>/', views.product, name="product"),  
     path('create-product/', views.createProduct, name="create-product"),
+    path('update-product/<slug:slug>/', views.updateProduct, name="update-product"),
+    path('delete-product/<slug:slug>/', views.deleteProduct, name="delete-product"),
 ]
--- store/views.py
+++ store/views.py
@@ -24,3 +24,28 @@ def createProduct(request):
             return redirect('store')
     context = {'form': form}
     return render(request, "store/store-form.html", context)
+
+def updateProduct(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        return render(request, '404.html') 
+    form = ProductForm(instance = product)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance = product)
+        if form.is_valid():
+            form.save()
+            return redirect('store')
+    context = {'form': form}
+    return render(request, "store/store-form.html", context)
+
+def deleteProduct(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        return render(request, '404.html') 
+    if request.method == "POST":
+        product.delete() 
+        return redirect('store')
+    context = {'object': product}
+    return render(request, "store/delete-product.html", context)
